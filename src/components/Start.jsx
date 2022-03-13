66991606cfde78321cd4cd71d2fba7cce0b51e79
Soohyeun <soohyeunp@gmail.com>
2022-03-12 20:46:32 -0800

    Add styles for landing page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,16 +1,21 @@
 import { useRef } from "react"
 
-export default function Start({setUserName}) {
+export default function Start({ setUserName }) {
     const inputRef = useRef();
 
     const handleClick = () => {
-        inputRef.current.value && setUserName(inputRef.current.value);        
+        inputRef.current.value && setUserName(inputRef.current.value);
     };
     return (
         <div className="start">
-            <input placeholder="enter your name" className="startInput" ref={inputRef} />
-            <button className="startButton" onClick={handleClick}>
-                Start</button>
+            <div className="logo">
+                    ...
+            </div>
+            <div className="inputValue">
+                <input placeholder="enter your name" className="startInput" ref={inputRef} />
+                <button className="startButton" onClick={handleClick}>
+                    Start</button>
+            </div>
         </div>
     )
 }
